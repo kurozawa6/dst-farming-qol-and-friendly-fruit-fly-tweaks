5a556b1606a7a462320c915e7478f053a235a689
kurozawa6 <75070724+kurozawa6@users.noreply.github.com>
2022-06-28 00:08:01 +0800

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -1,16 +1,22 @@
 local SetSharedLootTable = GLOBAL.SetSharedLootTable
 
+local function inChanceLootTable(loot, table)
+	for k, v in pairs(table) do
+		if v["prefab"] == loot then
+			return true
+		end
+	end
+	return false
+end
+
 local function AddFruitFlyFruitLoot(inst)
 	if inst.components.lootdropper ~= nil then
-		local mystery = 'cutgrass'
-		if inst.components.lootdropper.chanceloot[1]["prefab"] == "fruitflyfruit" then
-			mystery = 'powcake'
-		else
-			mystery = 'cutgrass'
+		if inChanceLootTable("fruitflyfruit", inst.components.lootdropper.chanceloot) then
+			return
 		end
 		local items = {
-					{'plantmeat',             1.00},
-					{mystery,         1.00},
+					{'plantmeat',     1.00},
+					{'fruitflyfruit', 1.00},
 				}
 		SetSharedLootTable(inst, items)
 		inst.components.lootdropper:SetChanceLootTable(inst)
